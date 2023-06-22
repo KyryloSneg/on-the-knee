a7d29f38cb388898b61bb6b09b925471d586bcac
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-06-22 17:16:51 +0300

    deleted unnecesarry import in the App.js file and added some general styles into App.css stylesheets

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import AppRouter from "./components/AppRouter";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <Navbar />
+      </header>
       <AppRouter />
     </BrowserRouter>
   );
