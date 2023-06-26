c20542f2412d251ba3a4456c118024ac5e3e6e91
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-06-26 16:14:19 +0300

    decomposed some component's logic into separate hooks

---
+++ client/src/hooks/useClickOutsideOptions.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export default function useClickOutsideOptions(hide) {
+  useEffect(() => {
+    
+    function clickHandler() {
+      hide();
+    }
+
+    window.addEventListener("click", clickHandler);
+
+    return () => {
+      window.removeEventListener("click", clickHandler);
+    }
+  });
+}
---
+++ client/src/hooks/useWindowWidth.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export default function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth);
+
+  useEffect(() => {
+    function resizeHandler() {
+      setWindowWidth(document.documentElement.clientWidth);
+    }
+
+    window.addEventListener("resize", resizeHandler)
+    return () => {
+      window.removeEventListener("resize", resizeHandler)
+    }
+  })
+
+  return windowWidth;
+}
