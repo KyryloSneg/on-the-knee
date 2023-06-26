1570bb15ad7483ee2bf489f645aa53e5815bf888
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-06-26 16:15:29 +0300

    added filters store

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createContext } from 'react';
 import UserStore from './stores/UserStore';
 import App from './App';
+import FiltersStore from './stores/FiltersStore';
 
 export const Context = createContext(null);
 
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <Context.Provider value={{
       user: new UserStore(),
+      filtersStore: new FiltersStore(),
     }}>
       <App />
     </Context.Provider>
