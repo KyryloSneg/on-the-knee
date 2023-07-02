6464077e8e08fb11b492c74a27b63e2554bdcb6e
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-07-02 17:51:45 +0300

    write some basic tests for the used filters section

--- client/src/components/UsedFilters.jsx
+++ client/src/components/UsedFilters.jsx
@@ -4,7 +4,7 @@ import "./styles/UsedFilters.css";
 
 const UsedFilters = observer(() => {
   return (
-    <section className="used-filters">
+    <section className="used-filters" data-testid="used-filters-section">
       <h2>Used filters</h2>
       <FiltersList />
     </section>
