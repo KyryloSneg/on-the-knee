b2ca5f1e0eee78f75c9917ff2d4cc34192cadafa
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-07-17 19:19:59 +0300

    Revert "moved all device images and thumbnail to color.items[i]' fields"
    This reverts commit 2908512ff7d8ce87b28f8ef1ad3e302944b52a85.

--- client/jsonServer/utils/createDeviceColor.js
+++ client/jsonServer/utils/createDeviceColor.js
@@ -1,17 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const { POSSIBLE_COLORS } = require("./consts");
 
-module.exports = function createDeviceColor(colorsArray, colorsId, deviceId, images, thumbnail) {
+module.exports = function createDeviceColor(colorsArray, colorsId, deviceId) {
   const colors = {
     "id": colorsId,
     "deviceId": deviceId,
-    "items": POSSIBLE_COLORS[faker.number.int({min: 0, max: POSSIBLE_COLORS.length - 1})].map(item => {
-      return {
-        ...item,
-        "images": images,
-        "thumbnail": thumbnail,
-      };
-    }),
+    "items": POSSIBLE_COLORS[faker.number.int({min: 0, max: POSSIBLE_COLORS.length - 1})],
   }
 
   colorsArray.push(colors);
