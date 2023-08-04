2fefe63597cb9c23e6f68c68bec893e39e71264d
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-08-04 18:23:16 +0300

    deleted previous json server to make new one

--- client/jsonServer/index.js
+++ client/jsonServer/index.js
@@ -1,39 +1,7 @@
-const createDevices = require("./utils/createDevices");
-const createUsers = require("./utils/createUsers");
-
 module.exports = () => {
   const data = {
-    "users": [],
-    "carts": [], 
-    "cartDevices": [/* {id, cartId, deviceId} */], // will be empty
-    "favorites": [], 
-    "favoriteDevices": [/* {id, favoritesId, deviceId} */], // will be empty
-    "comparison": [], 
-    "comparisonDevices": [/* {id, comparisonId, deviceId} */], // will be empty
-    "devices": [],
-    "deviceInfos": [],
-    "ratings": [],
-    "colors": [],
-    "brands": [],
-    "categories": []
-  }
 
-  const { users, carts, favorites, comparison } = createUsers();
-
-  createDevices()
-    .then(returnData => {
-      data.users = users;
-      data.carts = carts;
-      data.favorites = favorites;
-      data.comparison = comparison;
-
-      data.devices = returnData.devices;
-      data.deviceInfos = returnData.infos;
-      data.ratings = returnData.ratings;
-      data.colors = returnData.colors;
-      data.brands = returnData.brands;
-      data.categories = returnData.categories
-    });
+  };
 
   return data;
 }
--- client/jsonServer/utils/createMpn.js
+++
@@ -1,5 +0,0 @@
-const { faker } = require('@faker-js/faker');
-
-module.exports = (baseMpn) => {
-  return `${baseMpn}-${faker.number.int({min: 100000, max: 999999})}`;
-}
