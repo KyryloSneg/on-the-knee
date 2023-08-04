5b7ea0627d784e9c9ad9ee3cfdcd6d1bb76dcc9b
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-08-04 19:02:50 +0300

    created some fields for json server to fill in

--- client/jsonServer/index.js
+++ client/jsonServer/index.js
@@ -1,6 +1,51 @@
 module.exports = () => {
   const data = {
+    "devices": [],
+    "device-combinations": [],
+    "stocks": [],
+
+    "attributes": [],
+    "attribute-names": [],
+    "attribute-values": [],
+
+    "sales": [],
+    "sale-devices": [],
+    "sale-types": [],
+    "sale-type-names": [],
+
+    "categories": [],
+    "brands": [],
+    "deviceInfos": [],
+    "payment-infos": [],
     
+    "additional-services": [],
+    "additional-service-devices": [],
+    
+    "sellers": [],
+    "seller-feedbacks": [],
+    "seller-feedback-replies": [],
+
+    "carts": [],
+    "cart-devices": [],
+
+    "desired-lists": [],
+    "desired-devices": [],
+
+    "countries": [],
+    "regions": [],
+    "districts": [],
+    "cities": [],
+    "streets": [],
+
+    "store-locations": [],
+    "courier-schedules": [],
+
+    "receivents": [],
+    "device-feedbacks": [],
+    "device-feedback-replies": [],
+
+    "orders": [],
+    "order-device-combinations": [],
   };
 
   return data;
