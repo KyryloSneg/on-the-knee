9dddf95b1cfe482c9d397385c252c89f64061b43
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-08-12 17:01:39 +0300

    created countries, regions etc. until streets and store pickup points

---
+++ client/jsonServer/http/mapDataManagerAPI.js
@@ -0,0 +1,28 @@
+const { default: axios } = require("axios")
+
+async function getLocationCoords(country, region, district, city, street, houseNumber) {
+  let location = [country, region, district, city, street, houseNumber];
+  location = location.join(" ");
+
+  let response;
+  try {
+    const { data } = await axios.get("https://www.mapquestapi.com/geocoding/v1/address", {
+      "params": {
+        "key": process.env.MAPQUERY_API_KEY,
+        "location": location,
+      }
+    });
+
+    response = data;
+  } catch(e) {
+    console.log(e.message);
+  }
+
+
+  const { lat, lng } = response.results[0].locations[0].displayLatLng;
+  return { lat, lng };
+}
+
+module.exports = {
+  getLocationCoords,
+}
