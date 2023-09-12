726c9a076dc8fab7fade14e71b60b3eb0cac1ed3
KyryloSneg <podhvatilinkirill511@gmail.com>
2023-09-12 19:11:28 +0300

    added two skip btns in aside: one that will skip to the future device section and other one that skips to the beginning of aside bar ('remove all filter btn' or first filter category block btn). besides, i added some tests for the second button (todo: write first skip btn tests later)

--- client/src/components/UI/skipToNextPageContent/SkipTonextPageContent.jsx
+++ client/src/components/UI/skipToNextPageContent/SkipTonextPageContent.jsx
@@ -1,12 +1,17 @@
-const SkipTonextPageContent = ({ title, toFocusRef }) => {
+const SkipToNextPageContent = ({ title, toFocusRef, testId = null, ...props }) => {
+  let className = "skip-to-next-page-content";
+  if (props.className) {
+    className += ` ${props.className}`;
+  }
 
   function onClick() {
     // to focus ref element
-    toFocusRef.current.focus();
+    // adding "?." to not throw an error when a ref is not implemented atm 
+    toFocusRef?.current?.focus();
   }
 
   return (
-    <button className="skip-to-next-page-content" onClick={onClick}>
+    <button className={className} onClick={onClick} data-testid={`${testId}`}>
       <p>{title}</p>
       <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
         <path d="m14 18-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45 14 6l6 6Z" />
@@ -15,4 +20,4 @@ const SkipTonextPageContent = ({ title, toFocusRef }) => {
   );
 }
 
-export default SkipTonextPageContent;
+export default SkipToNextPageContent;
--- client/src/stores/AppStore.js
+++ client/src/stores/AppStore.js
@@ -4,6 +4,9 @@ class AppStore {
   constructor() {
     this._darkBgVisible = false;
     this._isBlockedScroll = false;
+    // some important app refs below to skip a big amount of props passing
+    this._deviceSectionRef = null;
+    this._asideBeginningRef = null;
     makeAutoObservable(this);
   }
 
@@ -14,6 +17,14 @@ class AppStore {
   setIsBlockedScroll(bool) {
     this._isBlockedScroll = bool;
   }
+
+  setDeviceSectionRef(ref) {
+    this._deviceSectionRef = ref;
+  }
+
+  setAsideBeginningRef(ref) {
+    this._asideBeginningRef = ref;
+  }
   
   get darkBgVisible() {
     return this._darkBgVisible;
@@ -21,6 +32,14 @@ class AppStore {
 
   get isBlockedScroll() {
     return this._isBlockedScroll;
+  }
+
+  get deviceSectionRef() {
+    return this._deviceSectionRef;
+  }
+
+  get asideBeginningRef() {
+    return this._asideBeginningRef;
   }
 }
 
