969d4aa4baffba96c504d80fc128c0b82737a12a
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-04-14 19:30:32 +0300

    fixed many bugs, made component easier to read and to maintain in future (changed form's controls and render of search results)

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useRef } from "react";
 import { Context } from "../Context";
-import useClosingAllWindows from "../hooks/useClosingAllWindows";
 
 const MainPage = () => {
   const { app } = useContext(Context);
@@ -10,7 +9,6 @@ const MainPage = () => {
     app.setPageRef(pageRef);
   }, [app]);
 
-  useClosingAllWindows();
   return (
     <div>
       MainPage
