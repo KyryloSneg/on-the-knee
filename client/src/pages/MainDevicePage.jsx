2e2e77747ea38b019a04e98c6b352d35900849bc
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-05-08 19:45:57 +0300

    created easy layout of main device page before making actual components for it

--- client/src/pages/MainDevicePage.jsx
+++ client/src/pages/MainDevicePage.jsx
@@ -1,7 +1,20 @@
+import "./styles/MainDevicePage.css";
+import DeviceImagesCarousel from "../components/DeviceImagesCarousel";
+import DeviceInfoSection from "../components/DeviceInfoSection";
+import DeviceRightDescription from "../components/DeviceRightDescription";
+import CommentsSection from "../components/UI/commentsSection/CommentsSection";
+
 const MainDevicePage = () => {
   return (
-    <section>
-      Main device page
+    <section className="main-device-page">
+      <div className="dev-images-description-wrap">
+        <DeviceImagesCarousel />
+        <DeviceRightDescription />
+      </div>
+      <div className="dev-info-comments-wrap">
+        <DeviceInfoSection />
+        <CommentsSection type="device" />
+      </div>
     </section>
   );
 }
