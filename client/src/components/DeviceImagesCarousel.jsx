104e347533045ad1730f8c3f60833be75909df8d
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-05-08 19:45:57 +0300

    created easy layout of main device page before making actual components for it

---
+++ client/src/components/DeviceImagesCarousel.jsx
@@ -0,0 +1,11 @@
+import "./styles/DeviceImagesCarousel.css";
+
+const DeviceImagesCarousel = () => {
+  return (
+    <section className="device-page-section">
+      DeviceImagesCarousel
+    </section>
+  );
+}
+
+export default DeviceImagesCarousel;
