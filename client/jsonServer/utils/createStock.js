43f8982cfad3b934a7ee8769dcc265598041c06a
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-05-17 19:39:36 +0300

    created device right description heading

--- client/jsonServer/utils/createStock.js
+++ client/jsonServer/utils/createStock.js
@@ -1,11 +1,13 @@
 const { faker } = require("@faker-js/faker");
 
-module.exports = (deviceCombinationId, stocks) => {
+module.exports = (device, deviceCombinationId, stocks) => {
   const isZero = faker.datatype.boolean(0.2);
-  const totalStock = isZero ? faker.number.int({ min: 1, max: 100 }) : 0;
+  const totalStock = isZero && !device.isPreOrder ? faker.number.int({ min: 1, max: 100 }) : 0;
 
   let stockStatus;
-  if (totalStock === 0) {
+  if (device.isPreOrder) {
+    stockStatus = "Awaiting"
+  } else if (totalStock === 0) {
     stockStatus = "Out of stock";
   } else if (totalStock <= 15) {
     stockStatus = "Is running out";
