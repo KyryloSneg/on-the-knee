cc30118a65a6fc3a75a00fe4ef8486bf1ceafb60
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-05-25 19:37:07 +0300

    created big cities section in the modal window

--- client/src/components/SelectUserLocationModalBigCities.jsx
+++ client/src/components/SelectUserLocationModalBigCities.jsx
@@ -1,9 +1,24 @@
+import { useContext } from "react";
 import "./styles/SelectUserLocationModalBigCities.css";
+import { Context } from "../Context";
 
 const SelectUserLocationModalBigCities = ({ bigCities }) => {
+  const { app } = useContext(Context);
+
+  function onClick(location) {
+    localStorage.setItem("location", JSON.stringify(location));
+    app.setUserLocation(location);
+  }
+
   return (
     <ul className="select-user-location-modal-big-cities">
-      
+      {bigCities.map(city => 
+        <li key={city.id}>
+          <button onClick={() => onClick(city)}>
+            {city.name}
+          </button>
+        </li>
+      )}
     </ul>
   );
 }
