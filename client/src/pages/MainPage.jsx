605ab2fc684de83b9cf76e14100dd163078616ab
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-05-30 18:00:31 +0300

    added user location btn to device right desc

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useRef } from "react";
 import { Context } from "../Context";
-import UserLocationBtn from "../components/UserLocationBtn";
 import UserLocationBtnNotification from "../components/UserLocationBtnNotification";
 
 const MainPage = () => {
@@ -14,7 +13,6 @@ const MainPage = () => {
   return (
     <div>
       MainPage
-      {/* <UserLocationBtn /> */}
       <UserLocationBtnNotification />
     </div>
   );
