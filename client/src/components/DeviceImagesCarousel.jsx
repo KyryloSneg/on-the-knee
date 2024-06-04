6676af499f2be9ab37bdf7bd8db224d294acbd99
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-06-04 16:29:30 +0300

    created device carousel sticky

--- client/src/components/DeviceImagesCarousel.jsx
+++ client/src/components/DeviceImagesCarousel.jsx
@@ -1,3 +1,4 @@
+import "./styles/DeviceImagesCarousel.css";
 import ImagesCarousel from "./UI/imagesCarousel/ImagesCarousel";
 
 const DeviceImagesCarousel = ({ device, selectedCombination, textSaleTypes = [], logoSaleTypes = [] }) => {
@@ -5,15 +6,17 @@ const DeviceImagesCarousel = ({ device, selectedCombination, textSaleTypes = [],
   const images = selectedCombination.images;
 
   return (
-    <ImagesCarousel 
-      type="device" 
-      images={images} 
-      className="device-page-section device-images-carousel" 
-      device={device} 
-      textSaleTypes={textSaleTypes}
-      logoSaleTypes={logoSaleTypes}
-      toResetOnLocationChange={true}
-    />
+    <div className="images-carousel-wrapper">
+      <ImagesCarousel
+        type="device"
+        images={images}
+        className="device-page-section device-images-carousel"
+        device={device}
+        textSaleTypes={textSaleTypes}
+        logoSaleTypes={logoSaleTypes}
+        toResetOnLocationChange={true}
+      />
+    </div>
   );
 }
 
