45622ea4f62f39c1b974ba2078fe48c6393a43d2
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-07-12 19:23:07 +0300

    created link to seller feedbacks from device page

--- client/src/components/DeviceSellerBlock.jsx
+++ client/src/components/DeviceSellerBlock.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
 import "./styles/DeviceSellerBlock.css";
-import { SELLER_ROUTE } from "../utils/consts";
+import { Link } from "react-router-dom";
+import { SELLER_FEEDBACKS_ROUTE, SELLER_ROUTE } from "../utils/consts";
+import StarRating from "./UI/starRating/StarRating";
 
 const DeviceSellerBlock = ({ seller }) => {
-  let to = SELLER_ROUTE + `${seller.id}--${seller.slug}`;
+  let sellerPageTo = SELLER_ROUTE + `${seller.id}--${seller.slug}`;
+  let sellerFeedbacksPageTo = SELLER_FEEDBACKS_ROUTE.replace(":sellerIdSlug", `${seller.id}--${seller.slug}`);
 
   return (
-    <p className="device-seller-block">
-      <span>Seller:</span>
-      <Link to={to} className="link-colors">{seller.name}</Link>
-    </p>
+    <div className="device-seller-block">
+      <p>
+        <span>Seller:</span>
+        <Link to={sellerPageTo} className="link-colors">{seller.name}</Link>
+      </p>
+      <div className="device-seller-block-rating-wrap">
+        <StarRating readOnlyValue={1} maxValue={1} width={20} height={20} />
+        <p>{seller.rating}</p>
+        <Link to={sellerFeedbacksPageTo} className="link-colors">
+          ({seller.rating === 0 ? "write a feedback" : "feedbacks"})
+        </Link>
+      </div>
+    </div>
   );
+
+  // return (
+  //   <p className="device-seller-block">
+  //     <span>Seller:</span>
+  //     <Link to={to} className="link-colors">{seller.name}</Link>
+  //   </p>
+  // );
 }
 
 export default DeviceSellerBlock;
