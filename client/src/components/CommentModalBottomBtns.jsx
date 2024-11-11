ee25e07b603e3c69e70252a3200d7e5a6bd9cdb8
kyrylosneg <podhvatilinkirill511@gmail.com>
2024-11-11 18:58:27 +0200

    removed seller's and seller manager's possibility to remain a device feedback, device question and to ask a seller

--- client/src/components/CommentModalBottomBtns.jsx
+++ client/src/components/CommentModalBottomBtns.jsx
@@ -1,11 +1,20 @@
 import "./styles/CommentModalBottomBtns.css";
 import UIButton from "./UI/uiButton/UIButton";
 
-const CommentModalBottomBtns = ({ type, setIsAnonymously, isAnonymously, closeModal, areErrors, areInputsBlocked }) => {
+const CommentModalBottomBtns = ({ 
+  type, setIsAnonymously, isAnonymously, closeModal,
+  areErrors, areInputsBlocked, isToShowSellerCantRemainComment 
+}) => {
   let checkboxDivClass = "checkbox-div";
   if (isAnonymously) {
     checkboxDivClass += " checked";
   }
+
+  const sellerCantRemainCommentMsg = (
+    type === "askSeller"
+      ? `A seller or a seller manager can't ask any seller`
+      : `A seller or a seller manager can't remain a ${type}`
+  );
 
   return (
     <div className="comment-modal-bottom-btns">
@@ -21,6 +30,11 @@ const CommentModalBottomBtns = ({ type, setIsAnonymously, isAnonymously, closeMo
           Send {type} anonymously
         </button>
       }
+      {isToShowSellerCantRemainComment && (
+        <p className="modal-seller-cant-remain-comment-error-msg">
+          {sellerCantRemainCommentMsg}
+        </p>
+      )}
       <div className="comment-modal-form-btn-group">
         <UIButton
           variant="modal-deny"
