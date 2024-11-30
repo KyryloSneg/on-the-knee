aebd4becf64aa6c7e7dd277d68f63cc1a181b52f
KyryloSneg <podhvatilinkirill511@gmail.com>
2024-11-30 18:49:03 +0200

    blocking scroll now sets overflow-y: scroll to the body elem only if the page is scrollable

--- client/src/hooks/useBlockingScroll.js
+++ client/src/hooks/useBlockingScroll.js
@@ -1,19 +1,27 @@
 import { useEffect } from "react";
+import getScrollHeight from "utils/getScrollHeight";
 
 function useBlockingScroll(isBlockedScroll) {
   useEffect(() => {
+    const isPageWithScroll = getScrollHeight() !== document.documentElement.clientHeight;
+
     if (isBlockedScroll) {
       // setting left and top css properties to not reset current page scroll
       document.body.style.left = window.scrollX + "px";
       document.body.style.top = -(window.scrollY) + "px";
+
       document.body.classList.add("global-blocked-scroll");
+      if (isPageWithScroll) document.body.classList.add("scroll-y");
     } else {
       const left = +document.body.style.left.replace("px", "");
       const top = +document.body.style.top.replace("px", "");
 
       document.body.style.removeProperty("left");
       document.body.style.removeProperty("top");
+
       document.body.classList.remove("global-blocked-scroll");
+      document.body.classList.remove("scroll-y");
+
       window.scrollTo({
         left: left,
         top: -top,
---
+++ client/src/utils/getScrollHeight.js
@@ -0,0 +1,9 @@
+export default function getScrollHeight() {
+  const scrollHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
+
+  return scrollHeight
+}
